410f0d95e577ad68b85e555a4f25ea47a6761579
SamehSheref <samehsheref40@gmail.com>
2025-07-15 20:08:40 +0300

    stage 5

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,11 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right',
+      timeOut: 3000,
+    }),
   ]
 };
